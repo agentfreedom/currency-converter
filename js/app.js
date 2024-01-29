ef72b492a8f2b35f3887b45b06bf83603dcfc9e6
Avdasov Oleksandr <agentfreedom@zoho.com>
2024-01-29 07:22:00 +0200

    fix: replace quotes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const apiKey = '1d9883052071a8ca45329fea';
-let api = 'https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD';
+let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 
 const fromDropDown = document.getElementById('from-currency-select');
 const toDropDown = document.getElementById('to-currency-select');
