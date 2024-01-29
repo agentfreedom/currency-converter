42cdecf90c78387d75b03c8c98c55e0cbfd6575d
Avdasov Oleksandr <agentfreedom@zoho.com>
2024-01-29 02:21:41 +0200

    feat: add created dropdown from currencies array function

--- js/app.js
+++ js/app.js
@@ -3,3 +3,17 @@ let api = 'https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD';
 
 const fromDropDown = document.getElementById('from-currency-select');
 const toDropDown = document.getElementById('to-currency-select');
+
+currencies.forEach((currency) => {
+  const option = document.createElement('option');
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
+});
+
+currencies.forEach((currency) => {
+  const option = document.createElement('option');
+  option.value = currency;
+  option.text = currency;
+  toDropDown.add(option);
+});
